7732d3351a8c3e65a14ccf3df3fadd66ddac1f19
kaio eduardo <kaiocarvalhode@gmail.com>
2024-09-06 20:57:09 -0300

    adicionando ao servidor

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -89,7 +89,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://chat-ztalecare.onrender.com")
     websocket.onmessage = processMessage
 }
 
